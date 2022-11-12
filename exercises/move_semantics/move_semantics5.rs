367d1ad01c9cb362817a6ff86e452752931eda68
freekeeperlzt <freekeeper@mail.ustc.edu.cn>
2022-11-12 19:50:19 +0800

    finish exercise move_semantics

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -3,13 +3,15 @@
 // adding, changing or removing any of them.
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
     let mut x = 100;
     let y = &mut x;
-    let z = &mut x;
     *y += 100;
+    // y的生命周期没有结束，为什么可以同时声明两个可变引用
+    let z = &mut x;
+    
     *z += 1000;
     assert_eq!(x, 1200);
 }
+
--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -2,24 +2,48 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand for a hint.
 // You can't change anything except adding or removing references.
 
-// I AM NOT DONE
 
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(&data);
 
-    string_uppercase(&data);
+    string_uppercase(data);
 }
 
 // Should not take ownership
-fn get_char(data: String) -> char {
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
-
+fn string_uppercase(mut data: String) {
+    /* 方式一 */
+    data = data.to_uppercase();
+    
+    /* 方式二 */
+    // let data = &data.to_uppercase();
+    
     println!("{}", data);
 }
+
+/* 方式三
+fn main() {
+    let mut data = "Rust is great!".to_string();
+
+    get_char(&data);
+
+    string_uppercase(&mut data);
+}
+
+// Should not take ownership
+fn get_char(data: &String) -> char {
+    data.chars().last().unwrap()
+}
+
+// Should take ownership
+fn string_uppercase(mut data: &mut String) {
+    *data = data.to_uppercase();
+    println!("{}", data);
+}
+*/
